8d92c869ed746a1275f4f5d6e0aa3516e3808076
anton <anton.malakaev@gmail.com>
2024-01-27 01:33:32 +0300

    time check

--- app/bot.py
+++ app/bot.py
@@ -2,10 +2,12 @@ from loader import dp, bot
 import asyncio
 from main_menu import set_menu
 import logging
+import datetime
 
 
 async def main():
     from handlers import dp
+    print("System time in Python app:", datetime.datetime.now())
     logging.basicConfig(
         level=logging.INFO,
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
