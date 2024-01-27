05de332f76f548750e39d7011873098c9f1f7bae
anton <anton.malakaev@gmail.com>
2024-01-28 02:08:32 +0300

    scheduler fix

--- app/bot.py
+++ app/bot.py
@@ -2,8 +2,8 @@ import asyncio
 import logging
 from main_menu import set_menu
 from config import LESSONS_TIMINGS
-from loader import dp, bot#, scheduler
-# from lib.notifications import add_scheduled_jobs
+from loader import dp, bot, scheduler
+from lib.notifications import add_scheduled_jobs
 
 
 async def main():
@@ -12,8 +12,8 @@ async def main():
         level=logging.INFO,
         format='%(filename)s:%(lineno)d #%(levelname)-8s '
                '[%(asctime)s] - %(name)s - %(message)s')
-    # add_scheduled_jobs(scheduler, LESSONS_TIMINGS)
-    # scheduler.start()
+    add_scheduled_jobs(scheduler, LESSONS_TIMINGS)
+    scheduler.start()
     await set_menu(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
