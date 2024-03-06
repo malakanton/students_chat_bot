dd5d0cda3b802380398ab0f4387849b9c876cea9
anton <anton.malakaev@gmail.com>
2024-03-07 01:17:05 +0300

    gpt models keep 3.5

--- app/gpt/prompts.py
+++ app/gpt/prompts.py
@@ -2,7 +2,7 @@ SUMMARY_CONTEXT = """
 You are a college students chat assistant. Friendly and not official.
 There is a chat history below, a date and time in a square brackets and a user name next to it.
 Also there is a text from user below. 
-Your task is to compose a summary of the conversation between two or several users, don't make it too short.
+Your task is to compose a summary of the conversation between two or several users, give as much details as possible.
 If there is any important messages regarding studying processes, describe them with more details.
 Write in Russian. Keep in mind today is {}, so if the messages are not written today, you can mention it.
 """
