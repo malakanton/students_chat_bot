97cc0b23f4fd402bebaad645efd523ffdec48b3a
anton <anton.malakaev@gmail.com>
2024-04-06 21:38:50 +0300

    update subjects.txt file added

--- app/handlers/groups/update_knowlage.py
+++ app/handlers/groups/update_knowlage.py
@@ -10,7 +10,7 @@ from lib.misc import prep_markdown
 from handlers.routers import groups_router
 from loader import vector_db, gd, embeddings, conn_str, bot
 from gpt.vector_db import DocumentsHandler
-from config import CONN_STRING, INFO_COLLECTION, ADMIN_CHAT
+from config import SUBJECTS_COLLECTION, INFO_COLLECTION, ADMIN_CHAT
 
 
 @groups_router.message(
@@ -19,25 +19,39 @@ from config import CONN_STRING, INFO_COLLECTION, ADMIN_CHAT
             )
 async def update_knowlage(message: Message):
     await bot.send_chat_action(message.chat.id, "upload_document")
-    if update_collection():
+    if update_collection(INFO_COLLECTION):
         await message.answer(prep_markdown(lx.COLLECTION_UPDATED))
     else:
         await message.answer(prep_markdown(lx.COLLECTION_NOT_UPDATED))
     await message.delete()
 
 
-def update_collection() -> bool:
+@groups_router.message(
+            Command('subjects_update'),
+            F.chat.id == int(ADMIN_CHAT)
+            )
+async def update_subjects_triggers(message: Message):
+    await bot.send_chat_action(message.chat.id, "upload_document")
+    if update_collection(SUBJECTS_COLLECTION):
+        await message.answer(prep_markdown(lx.SUBJECTS_UPDATED))
+    else:
+        await message.answer(prep_markdown(lx.COLLECTION_NOT_UPDATED))
+    await message.delete()
+
+
+def update_collection(collection: str) -> bool:
     logger.info('Start vector collection updating')
-    file_path = gd.download_file('subjects_info.txt')
+    file_name = collection + '.txt'
+    file_path = gd.download_file(file_name)
     if not file_path:
-        logger.info('failed to download file subjects_info.txt')
+        logger.info(f'failed to download file {file_name}')
         return False
     docs = TextLoader(file_path).load()
     dh = DocumentsHandler(docs)
     vector_db.from_documents(
         embedding=embeddings,
-        documents=dh.prep_subjects_infos(),
-        collection_name=INFO_COLLECTION,
+        documents=dh.prep_subjects_infos(collection),
+        collection_name=collection,
         connection_string=conn_str,
         pre_delete_collection=True,
     )
