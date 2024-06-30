5a1757b2e1e82365085d844a0707d4fbc982b90f
anton <anton.malakaev@gmail.com>
2024-06-30 17:59:31 +0300

    config launch fix

--- app/lib/config/config.py
+++ app/lib/config/config.py
@@ -13,8 +13,11 @@ class Secrets:
 
     def _parse_dotenv(self) -> dict:
         attrs_dict = dict()
+
         if not os.path.exists(self._dotenv_path):
             self._dotenv_path = ".env"
+        print('Dotenv file:', self._dotenv_path)
+        print(os.getcwd())
         with open(self._dotenv_path) as dotenv_file:
             lines = dotenv_file.readlines()
 
